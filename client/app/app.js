b6d5f56f5d6612c1fc9959ff2bc6ca125fd2e6e5
ParvBhardwaj <bhparv@gmail.com>
2017-08-09 18:05:57 +0530

    .

--- client/app/app.js
+++ client/app/app.js
@@ -1,8 +1,20 @@
 'use strict';
 
 angular.module('movieMelaApp', ['movieMelaApp.auth', 'movieMelaApp.admin', 'movieMelaApp.constants',
-  'ngCookies', 'ngResource', 'ngSanitize', 'ngRoute', 'btford.socket-io', 'ui.bootstrap',
-  'validation.match', 'ui.filters'
+  'ngCookies', 'ngResource', 'ngSanitize', 'ngRoute', 'btford.socket-io',
+  'validation.match', 'ui.filters', 'ui.bootstrap'
+])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider.otherwise({
+      redirectTo: '/'
+    });
+
+    $locationProvider.html5Mode(true);
+  });
+
+
+angular.module('dhirFilmsApp', ['dhirFilmsApp.auth', 'dhirFilmsApp.admin', 'dhirFilmsApp.constants',
+  'ngCookies', 'ngResource', 'ngSanitize', 'ngRoute', 'btford.socket-io', 'validation.match'
 ])
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider.otherwise({
